b6946ed8262e2517230dfa7b2a50c16f895b3569
Vaibhav Aher1313 <Vaibhav_Aher1313@jabil.com>
2021-10-22 11:03:39 +0530

    ok

---
+++ StateCode2.4/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace StateCencuse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StateCensusAnalyser obj = new StateCensusAnalyser();
+            obj.readCSV();
+        }
+    }
+}
