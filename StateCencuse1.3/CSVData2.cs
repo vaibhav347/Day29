58529a6175cda1bb85553700fd3237153649191a
Vaibhav Aher1313 <Vaibhav_Aher1313@jabil.com>
2021-10-22 11:00:17 +0530

    ok

---
+++ StateCencuse1.3/CSVData2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StateCencuse
+{
+    class CSVData2
+    {
+        public string State { get; set; }
+        public string Population { get; set; }
+
+    }
+}
