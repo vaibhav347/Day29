6a71978c6ce81a78d8a6634a3c7d6afcdefb1ad7
Vaibhav Aher1313 <Vaibhav_Aher1313@jabil.com>
2021-10-22 11:01:40 +0530

    ok

---
+++ StateCode/CSVData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StateCencuse
+{
+    public class CSVData
+    {
+        public string State { get; set; }
+        public string Population { get; set; }
+    }
+}
