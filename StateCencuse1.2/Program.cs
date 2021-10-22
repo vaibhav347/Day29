67d6f4b1b507b090d9296d61de37b59e03ccc8b1
Vaibhav Aher1313 <Vaibhav_Aher1313@jabil.com>
2021-10-22 10:59:51 +0530

    ok

---
+++ StateCencuse1.2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace StateCencuse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StateCensusAnalyser obj = new StateCensusAnalyser();
+            try
+            {
+                obj.readCSV();
+            }
+            catch(RecordNotMatchException e)
+            {
+                Console.WriteLine(e.Message);
+
+            }
+        }
+    }
+}
---
+++ StateCencuse1.2/RecordNotMatchException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StateCencuse
+{
+    public class RecordNotMatchException:Exception
+    {
+        public RecordNotMatchException(string msg):base(msg)
+        {
+
+        }
+    }
+}
